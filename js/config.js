25b39b4aa903913cdaac8fda4976eb18201c7f19
ukonn_admin <fujiman8@gmail.com>
2019-03-16 13:38:18 +0900

    1st

---
+++ js/config.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAgr7SMzoC5hu3oOaXijk-bHI2bWRfeujs",
+    authDomain: "slcproject.firebaseapp.com",
+    databaseURL: "https://slcproject.firebaseio.com",
+    projectId: "slcproject",
+    storageBucket: "slcproject.appspot.com",
+    messagingSenderId: "1005272922111"
+  };
+  firebase.initializeApp(config);
